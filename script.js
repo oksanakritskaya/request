25e705bf82d95cc4ffcf3a37e233361cbac738d3
Oksana Kritskaya <Oksana.Kritskaya@sidenis.com>
2018-09-27 17:51:20 +0700

    kjh

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 (function() {
-    const api = 'https://typeahead-backend-crgxcwsytz.now.sh/';
+    let hotels = [];
+
+    const api = 'https://hotels-backend.now.sh/';
     fetch(api + 'api/hotels/1')
         .then(function(response) {
             console.log('Request succeeded with JSON response');
             return response.json();
         })
+        .then(function(data) {
+            hotels = data;
+            console.log(hotels);
+            renderHotels();
+        });
+
+
+    function renderHotels() {
+
+    }
 })();
